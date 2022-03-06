35e836208c5fdd21d3c5a94988f0bd834afeb143
allen3325 <00857146@email.ntou.edu.tw>
2022-03-06 16:08:03 +0800

    chore: first init

---
+++ src/main/java/ntou/notesharedevbackend/NoteShareDevBackendApplication.java
@@ -0,0 +1,13 @@
+package ntou.notesharedevbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteShareDevBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteShareDevBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ntou/notesharedevbackend/NoteShareDevBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ntou.notesharedevbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteShareDevBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
