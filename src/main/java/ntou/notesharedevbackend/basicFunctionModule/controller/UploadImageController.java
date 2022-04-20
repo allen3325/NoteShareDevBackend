d666b8525e19c0d820b2c585eaecdf5c31c79909
genewang0929 <genewang7@gmail.com>
2022-04-20 20:29:31 +0800

    feat- upload image init

---
+++ src/main/java/ntou/notesharedevbackend/basicFunctionModule/controller/UploadImageController.java
@@ -0,0 +1,16 @@
+package ntou.notesharedevbackend.basicFunctionModule.controller;
+
+import ntou.notesharedevbackend.entity.*;
+import ntou.notesharedevbackend.userModule.entity.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.*;
+
+import java.net.*;
+
+@RestController
+@RequestMapping(value = "/picture",produces = MediaType.APPLICATION_JSON_VALUE)
+public class UploadImageController {
+
+
+}
---
+++ src/main/java/ntou/notesharedevbackend/basicFunctionModule/service/UploadImageService.java
@@ -0,0 +1,10 @@
+package ntou.notesharedevbackend.basicFunctionModule.service;
+
+import ntou.notesharedevbackend.repository.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+
+@Service
+public class UploadImageService {
+
+}
