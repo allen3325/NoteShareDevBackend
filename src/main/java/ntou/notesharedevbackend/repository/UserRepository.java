099f187ebf600b6f164bb2ca36650f463b8b54d1
genewang <genewang7@gmail.com>
2022-05-19 11:40:43 +0800

    fix- search by keyword add pagination

--- src/main/java/ntou/notesharedevbackend/repository/UserRepository.java
+++ src/main/java/ntou/notesharedevbackend/repository/UserRepository.java
@@ -2,6 +2,7 @@ package ntou.notesharedevbackend.repository;
 
 import ntou.notesharedevbackend.folderModule.entity.Folder;
 import ntou.notesharedevbackend.userModule.entity.AppUser;
+import org.springframework.data.domain.*;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,9 @@ import java.util.List;
 @Repository
 public interface UserRepository  extends MongoRepository<AppUser,String> {
     AppUser findByEmail(String email);
-    List<AppUser> findByNameLike(String name);
+    List<AppUser> findAppUserByNameLike(String name);
+
+    Page<AppUser> findAppUserByNameLike(String name, Pageable pageable);
     @Query("{'folders.id': ?0}")
     AppUser findFolderById(String id);
     boolean existsByEmail(String email);
--- src/main/java/ntou/notesharedevbackend/searchModule/service/SearchService.java
+++ src/main/java/ntou/notesharedevbackend/searchModule/service/SearchService.java
@@ -1,19 +1,33 @@
 package ntou.notesharedevbackend.searchModule.service;
 
+import com.fasterxml.jackson.databind.*;
 import ntou.notesharedevbackend.repository.*;
 import ntou.notesharedevbackend.userModule.entity.*;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.*;
 
 import java.util.*;
 
 @Service
 public class SearchService {
+    private final int PAGE = 0;
+    private final int SIZE = 20;
+
     @Autowired
     private UserRepository userRepository;
 
     public AppUser[] getSearchedUser(String userName) {
-        List<AppUser> appUserLike = userRepository.findByNameLike(userName);
-        return appUserLike.toArray(new AppUser[0]);
+        PageRequest pageRequest = PageRequest.of(PAGE, SIZE, Sort.by("name").descending());
+        Page<AppUser> appUserLikePage = userRepository.findAppUserByNameLike(userName, pageRequest);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+//        List<AppUser> appUserList = userRepository.findAppUserByNameLike(userName);
+        List<AppUser> appUserList = appUserLikePage.getContent();
+        return appUserList.toArray(new AppUser[0]);
     }
+
+//    public String getSearchedByKeyword() {
+//
+//    }
 }
