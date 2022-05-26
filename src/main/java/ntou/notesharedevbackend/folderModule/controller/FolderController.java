2976fc0f6ae43bfd264c882fa963d31fd5efb390
allen3325 <00857146@email.ntou.edu.tw>
2022-05-27 04:43:40 +0800

    feat: Swagger API Docs generator in maven and gradle. feat: Folder module's API summary all done,

--- src/main/java/ntou/notesharedevbackend/folderModule/controller/FolderController.java
+++ src/main/java/ntou/notesharedevbackend/folderModule/controller/FolderController.java
@@ -1,5 +1,6 @@
 package ntou.notesharedevbackend.folderModule.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import ntou.notesharedevbackend.folderModule.entity.Folder;
 import ntou.notesharedevbackend.folderModule.entity.FolderRequest;
 import ntou.notesharedevbackend.folderModule.entity.FolderReturn;
@@ -25,6 +26,7 @@ public class FolderController {
         this.folderService = folderService;
     }
 
+    @Operation(summary = "Get All Folders From User.")
     @GetMapping("/{email}")
     public ResponseEntity<ArrayList<Folder>> getAllFoldersFromUser(@PathVariable(name = "email")String email){
         ArrayList<Folder> folders = folderService.getAllFoldersFromUser(email);
@@ -32,18 +34,21 @@ public class FolderController {
         return ResponseEntity.ok(folders);
     }
 
+    @Operation(summary = "Get All Folders and Notes Under User's specific folder.")
     @GetMapping("/{email}/{folderID}")
     public ResponseEntity<FolderReturn> getAllFoldersFromFolderID(@PathVariable(name = "email")String email, @PathVariable(name = "folderID")String folderID) {
         FolderReturn folders = folderService.getAllContentUnderFolderID(folderID);
         return ResponseEntity.ok(folders);
     }
 
+    @Operation(summary = "Can easily rename folder name.",description = "設想可以用在主畫面，點擊進入資料夾前對資料夾做簡易的編輯。")
     @PutMapping("/rename/{email}/{folderID}/{name}")
     public ResponseEntity<Folder> renameFolderByID(@PathVariable(name = "email")String email, @PathVariable(name = "folderID")String folderID,@PathVariable(name = "name")String name){
         Folder folder = folderService.renameFolderByID(email,folderID,name);
         return ResponseEntity.ok(folder);
     }
 
+    @Operation(summary = "Change folder's path.(!!!!attention!!!!,path should be correct)")
     @PutMapping("/save/{email}/{folderID}")
     public ResponseEntity<Folder> changePathByID(@PathVariable(name = "email")String email, @PathVariable(name =
             "folderID")String folderID,@RequestBody Folder request){
@@ -51,12 +56,14 @@ public class FolderController {
         return ResponseEntity.ok(folder);
     }
 
+    @Operation(summary = "Change favorite state.")
     @PutMapping("/favorite/{email}/{folderID}")
     public ResponseEntity favorite(@PathVariable(name = "email")String email, @PathVariable(name = "folderID")String folderID){
         folderService.setFavorite(email,folderID);
         return ResponseEntity.status(201).build();
     }
 
+    @Operation(summary = "Create a new folder.")
     @PostMapping("/{email}")
     public ResponseEntity<Folder> createFolder(@PathVariable(name = "email")String email,
                                                @RequestBody FolderRequest request){
@@ -71,6 +78,7 @@ public class FolderController {
         return ResponseEntity.created(location).body(folder);
     }
 
+    @Operation(summary = "Delete this folder and all children folder under this folder.")
     @DeleteMapping("/{email}/{folderID}")
     public ResponseEntity<String> deleteFolder(@PathVariable(name = "email")String email, @PathVariable(name = "folderID")String folderID){
         folderService.deleteFolderByID(email,folderID);
