93d49c526994281a1564092692864c848e489c10
yiting <00853129@email.ntou.edu.tw>
2022-06-13 01:09:52 +0800

    feat:User Test: wrongVerifyCode, ModifyUserStrength, ModifyUserProfile

--- src/test/java/ntou/notesharedevbackend/folderTest/FolderTest.java
+++ src/test/java/ntou/notesharedevbackend/folderTest/FolderTest.java
@@ -359,7 +359,7 @@ public class FolderTest {
 
     //TODO:改變收藏狀態 會移出嗎，Response $.res.msg?
     @Test
-    public void testChangeFavoriteStateOfFolder() throws Exception{
+    public void testPutIntoFavoriteFolder() throws Exception{
         //建User
         AppUser appUser = createUser();
         //建folder
@@ -384,7 +384,38 @@ public class FolderTest {
             throw new Exception("Folder Change Favorite State : folder does not put into favorite folder's children");
         }
     }
+//TODO:merge後測試
 
+//    @Test
+//    public void testTakeOutFromFavoriteFolder() throws Exception{
+//        //建User
+//        AppUser appUser = createUser();
+//        //建folder
+//        Folder folder = createFolder("Ting","/Buy/Ting",appUser.getFolders().get(0));
+//        Folder childrenFolder = createFolder("Alan","/Buy/Ting/Alan", folder.getId());
+//        Folder children2Folder = createFolder("Eva","/Buy/Ting/Eva", folder.getId());
+//        folder.getChildren().add(childrenFolder.getId());
+//        folder.getChildren().add(children2Folder.getId());
+//        folderRepository.save(folder);
+//        //放進Favorite folder
+//        Folder favoriteFolder = folderRepository.findById(appUser.getFolders().get(1)).get();
+//        favoriteFolder.getChildren().add(folder.getId());
+//        folderRepository.save(favoriteFolder);
+//        appUser.getFolders().add(folder.getId());
+//        appUser.getFolders().add(childrenFolder.getId());
+//        appUser.getFolders().add(children2Folder.getId());
+//        userRepository.insert(appUser);
+//        //收藏
+//        mockMvc.perform(put("/folder/favorite/"+appUser.getEmail()+"/"+folder.getId())
+//                        .headers(httpHeaders))
+//                .andExpect(status().isCreated())
+//                .andExpect(jsonPath("$.msg").value("success"));
+//        //檢查
+//        String favoriteFolderID =userRepository.findByEmail("yitingwu.1030@gmail.com").getFolders().get(1);
+//        if(folderRepository.findById(favoriteFolderID).get().getChildren().contains(folder.getId())){
+//            throw new Exception("Folder Change Favorite State : folder does not put into favorite folder's children");
+//        }
+//    }
     @Test
     public void testDeleteFolderByID () throws Exception{
         //建User
--- src/test/java/ntou/notesharedevbackend/userTest/UserTest.java
+++ src/test/java/ntou/notesharedevbackend/userTest/UserTest.java
@@ -1,9 +1,11 @@
 package ntou.notesharedevbackend.userTest;
 
+import com.google.gson.JsonArray;
 import ntou.notesharedevbackend.folderModule.entity.Folder;
 import ntou.notesharedevbackend.folderTest.FolderTest;
 import ntou.notesharedevbackend.repository.UserRepository;
 import ntou.notesharedevbackend.userModule.entity.AppUser;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,6 +24,7 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -187,6 +190,103 @@ public class UserTest {
                 .andExpect(jsonPath("$.headshotPhoto").value(appUser.getHeadshotPhoto()))
                 .andExpect(jsonPath("$.admin").value(appUser.isAdmin()))
                 .andExpect(jsonPath("$.activate").value(appUser.isActivate()));
+    }
+
+    //TODO:使用者可以編輯的資料
+    //@Test
+    public void testReplaceUser() throws Exception{
+        AppUser appUser = userRepository.findByEmail("yitingwu.1030@gmail.com");
+        String newName = "TestNewName";
+        String newPassword = "TestNewPassword";
+        String newProfile = "Test Profile"; //自我介紹
+        JSONArray newStrength=new JSONArray();//擅長科目
+        newStrength.put("Operating System");
+        newStrength.put("Java");
+        newStrength.put("Discrete mathematics");
+        JSONArray newSubscribe=new JSONArray();
+        JSONArray newBell=new JSONArray();
+        JSONArray newFans=new JSONArray();
+        Integer coin;
+        String headshotPhoto;
+        ArrayList<String> newStrengthArray = new ArrayList<>(); //擅長科目
+        newStrengthArray.add("Operating System");
+        newStrengthArray.add("Java");
+        newStrengthArray.add("Discrete mathematics");
+        ArrayList<String> newSubscribeArray= new ArrayList<>();
+        ArrayList<String> newBellArray = new ArrayList<>();
+        ArrayList<String> newFansArray = new ArrayList<>();
+    }
+
+    @Test
+    public void testModifyUserStrength() throws Exception{
+        AppUser appUser =userRepository.findByEmail("yitingwu.1030@gmail.com");
+        JSONArray newStrength =new JSONArray();
+        newStrength.put("Operating System");
+        newStrength.put("Java");
+        newStrength.put("Discrete mathematics");
+        ArrayList<String> newStrengthArray = new ArrayList<>();
+        newStrengthArray.add("Operating System");
+        newStrengthArray.add("Java");
+        newStrengthArray.add("Discrete mathematics");
+        JSONObject request = new JSONObject()
+                .put("strength",newStrength);
+
+        mockMvc.perform(put("/user/strength/"+appUser.getEmail())
+                        .headers(httpHeaders)
+                        .content(request.toString()))
+                .andExpect(status().isOk());
+        //TODO:response 統一
+
+//                .andExpect(jsonPath("$.id").value(appUser.getId()))
+//                .andExpect(jsonPath("$.email").value(appUser.getEmail()))
+//                .andExpect(jsonPath("$.name").value(appUser.getName()))
+//                .andExpect(jsonPath("$.verifyCode").value(appUser.getVerifyCode()))
+//                .andExpect(jsonPath("$.password").hasJsonPath())
+//                .andExpect(jsonPath("$.profile").value(appUser.getProfile()))
+//                .andExpect(jsonPath("$.strength").value(newStrength))
+//                .andExpect(jsonPath("$.folders").value(appUser.getFolders()))
+//                .andExpect(jsonPath("$.subscribe").value(appUser.getSubscribe()))
+//                .andExpect(jsonPath("$.bell").value(appUser.getBell()))
+//                .andExpect(jsonPath("$.fans").value(appUser.getFans()))
+//                .andExpect(jsonPath("$.coin").value(appUser.getCoin()))
+//                .andExpect(jsonPath("$.headshotPhoto").value(appUser.getHeadshotPhoto()))
+//                .andExpect(jsonPath("$.admin").value(appUser.isAdmin()))
+//                .andExpect(jsonPath("$.activate").value(appUser.isActivate()));
+        if(!userRepository.findByEmail("yitingwu.1030@gmail.com").getStrength().equals(newStrengthArray)){
+            throw new Exception("Modify User Strength : strength does not change");
+        }
+    }
+
+    @Test
+    public void testModifyUserProfile() throws Exception{
+        AppUser appUser =userRepository.findByEmail("yitingwu.1030@gmail.com");
+        String newProfile = "我是測試，我在測試，希望測試成功";
+        JSONObject request = new JSONObject()
+                .put("profile",newProfile);
+
+        mockMvc.perform(put("/user/profile/"+appUser.getEmail())
+                        .headers(httpHeaders)
+                        .content(request.toString()))
+                .andExpect(status().isOk());
+//                .andExpect(jsonPath("$.id").value(appUser.getId()))
+//                .andExpect(jsonPath("$.email").value(appUser.getEmail()))
+//                .andExpect(jsonPath("$.name").value(appUser.getName()))
+//                .andExpect(jsonPath("$.verifyCode").value(appUser.getVerifyCode()))
+//                .andExpect(jsonPath("$.password").hasJsonPath())
+//                .andExpect(jsonPath("$.profile").value(newProfile))
+//                .andExpect(jsonPath("$.strength").value(appUser.getStrength()))
+//                .andExpect(jsonPath("$.folders").value(appUser.getFolders()))
+//                .andExpect(jsonPath("$.subscribe").value(appUser.getSubscribe()))
+//                .andExpect(jsonPath("$.bell").value(appUser.getBell()))
+//                .andExpect(jsonPath("$.fans").value(appUser.getFans()))
+//                .andExpect(jsonPath("$.coin").value(appUser.getCoin()))
+//                .andExpect(jsonPath("$.headshotPhoto").value(appUser.getHeadshotPhoto()))
+//                .andExpect(jsonPath("$.admin").value(appUser.isAdmin()))
+//                .andExpect(jsonPath("$.activate").value(appUser.isActivate()));
+        //TODO:response 統一
+        if(!userRepository.findByEmail("yitingwu.1030@gmail.com").getProfile().equals(newProfile)){
+            throw new Exception("Modify User Profile : profile does not change");
+        }
     }
     @AfterEach
     public void clear(){
