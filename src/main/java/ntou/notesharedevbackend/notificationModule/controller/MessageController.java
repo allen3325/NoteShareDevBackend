4f29be988228e705fe8c9425deb222d65b5bc0eb
genewang <genewang7@gmail.com>
2022-06-24 14:04:07 +0800

    feat- add scheduled notification

--- src/main/java/ntou/notesharedevbackend/notificationModule/controller/MessageController.java
+++ src/main/java/ntou/notesharedevbackend/notificationModule/controller/MessageController.java
@@ -1,37 +1,39 @@
 package ntou.notesharedevbackend.notificationModule.controller;
-
 import ntou.notesharedevbackend.notificationModule.entity.*;
-import ntou.notesharedevbackend.notificationModule.service.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.messaging.handler.annotation.*;
-import org.springframework.messaging.simp.annotation.*;
+import org.springframework.messaging.simp.*;
+import org.springframework.scheduling.annotation.*;
 import org.springframework.stereotype.*;
-import org.springframework.web.util.*;
 
 import java.security.*;
 
+@EnableScheduling
 @Controller
 public class MessageController {
+    private final SimpMessagingTemplate messagingTemplate;
+
     @Autowired
-    private NotificationService notificationService;
+    public MessageController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
 
     @MessageMapping("/message/{postID}")
-    @SendTo("/topic/messages/{postID}")
-    public ResponseMessage getMessage(@DestinationVariable String postID, final Message message) throws InterruptedException {
+    public void getMessage(@DestinationVariable String postID, final Message message) throws InterruptedException {
         Thread.sleep(1000);
-        notificationService.sendGlobalNotification(postID);
-        return new ResponseMessage(HtmlUtils.htmlEscape(message.getMessageContent()));
+        messagingTemplate.convertAndSend("/topic/messages/" + postID, message);
+        messagingTemplate.convertAndSend("/topic/global-notifications/" + postID, "Global Notification");
     }
 
     @MessageMapping("/private-message")
-    @SendToUser("/topic/private-messages")
-    public ResponseMessage getPrivateMessage(final Message message,
-                                             final Principal principal) throws InterruptedException {
+    public void getPrivateMessage(final Message message, final Principal principal) throws InterruptedException {
         Thread.sleep(1000);
-        notificationService.sendPrivateNotification(principal.getName());
-        return new ResponseMessage(HtmlUtils.htmlEscape(
-                "Sending private message to user " + principal.getName() + ": "
-                        + message.getMessageContent())
-        );
+        messagingTemplate.convertAndSendToUser(principal.getName(), "/topic/private-messages", message);
+        messagingTemplate.convertAndSendToUser(principal.getName(),"/topic/private-notifications", "Private Notification");
+    }
+
+//    @Scheduled(fixedRate = 5000)
+    public void sendScheduledMessage() {
+        messagingTemplate.convertAndSend("/topic/scheduled-messages", "Hello World");
     }
 }
--- src/main/java/ntou/notesharedevbackend/notificationModule/entity/Message.java
+++ src/main/java/ntou/notesharedevbackend/notificationModule/entity/Message.java
@@ -1,13 +1,44 @@
 package ntou.notesharedevbackend.notificationModule.entity;
 
-public class Message {
-    private String messageContent;
+import lombok.*;
 
-    public String getMessageContent() {
-        return messageContent;
+@ToString
+public class Message {
+    private String user;
+    private String content;
+    private String time;
+
+    public Message() {
+
     }
 
-    public void setMessageContent(String messageContent) {
-        this.messageContent = messageContent;
+    public Message(Message message) {
+        this.user = message.getUser();
+        this.content = message.getContent();
+        this.time = message.getTime();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 }
