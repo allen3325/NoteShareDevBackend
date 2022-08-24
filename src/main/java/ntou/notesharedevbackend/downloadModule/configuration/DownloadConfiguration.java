f2ff9c94f069274f1d07a6bf3efe482c3c83054e
genewang <genewang7@gmail.com>
2022-08-24 23:14:13 +0800

    feat- download pdf done

---
+++ src/main/java/ntou/notesharedevbackend/downloadModule/configuration/DownloadConfiguration.java
@@ -0,0 +1,28 @@
+package ntou.notesharedevbackend.downloadModule.configuration;
+
+import org.springframework.boot.*;
+import org.springframework.context.annotation.*;
+
+import java.io.*;
+import java.nio.file.*;
+
+@Configuration
+public class DownloadConfiguration {
+    @Bean
+    ApplicationRunner applicationRunner() {
+        return args -> {
+            String homePath = System.getProperty("user.home");
+            String downloadPath = homePath + "/NoteShareDownload";
+            String htmlPath = downloadPath + "/note.html";
+            String cssPath  = downloadPath + "/note.css";
+            if (!Files.exists(Paths.get(downloadPath))) {
+                Files.createDirectories(Paths.get(downloadPath));
+                Files.createFile(Paths.get(htmlPath));
+                Files.createFile(Paths.get(cssPath));
+            }
+            // erase both files
+            new PrintWriter(htmlPath).close();
+            new PrintWriter(cssPath).close();
+        };
+    }
+}
