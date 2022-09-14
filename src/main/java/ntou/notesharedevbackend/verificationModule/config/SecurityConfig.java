9f73517b9ef10161c1de6387a79194ecccb491a6
MYWINDOWSPC\allen <00857146@email.ntou.edu.tw>
2022-09-14 22:52:04 +0800

    chore: add ninja to security list.

--- src/main/java/ntou/notesharedevbackend/verificationModule/config/SecurityConfig.java
+++ src/main/java/ntou/notesharedevbackend/verificationModule/config/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:3000/", "http://54.238.149.164:3000/", "https://54" +
-                ".238.149.164:3000/","https://noteshare.soselab.tw/"));
+                ".238.149.164:3000/","https://noteshare.soselab.tw/","http://noteshare.ninja/"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin", "Cache-Control", "Content-Type", "Authorization"));
