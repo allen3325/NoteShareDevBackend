d07150d7d570ce41fb3d9f2c67f6d71ca4f13fe6
allen3325 <00857146@email.ntou.edu.tw>
2023-03-06 15:33:09 +0800

    just for some test. ignore it.

--- src/main/java/ntou/notesharedevbackend/NoteShareDevBackendApplication.java
+++ src/main/java/ntou/notesharedevbackend/NoteShareDevBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NoteShareDevBackendApplication {
-
+// test
     public static void main(String[] args) {
         SpringApplication.run(NoteShareDevBackendApplication.class, args);
     }
